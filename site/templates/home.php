e1945bc2c3b5aea06f7b638838e9252d3cf84d48
Ruben Philipp <rubenphilipp@Rubens-MacBook-Pro-2.local>
2021-02-17 17:30:36 +0100

    first quick version of home page and footer

--- site/templates/home.php
+++ site/templates/home.php
@@ -2,7 +2,23 @@
 
 <body>
 
-	<h1 class="text-2xl"><?= $page->title() ?></h1>
+	<section class="text-center">
+		<img src="<?= asset('assets/images/UE.png')->url() ?>" class="w-32 h-auto inline-block">
+		<h1 class="text-2xl"><?= $site->title() ?></h1>
+		<div class="max-w-md inline-block leading-none text-sm mt-4">
+			<?= $page->descriptionShort()->kt() ?>
+		</div>
+	</section>
+
+	<main>
+		
+	</main>
+
+	<section>
+		
+	</section>
+
+	<?php snippet('section-footer') ?>
 	
 
 </body>
