fde13baaf0b5d966348eaddb354ca763b9234a8f
Ruben Philipp <rubenphilipp@Rubens-MacBook-Pro-2.local>
2021-02-17 21:35:37 +0100

    changed title in header snippet

--- site/snippets/section-header.php
+++ site/snippets/section-header.php
@@ -1,4 +1,4 @@
 <header class="inline-block w-full text-center">
 	<a href="<?= page('home')->url() ?>"><img src="<?= asset('assets/images/UE.png')->url() ?>" title="<?= $site->title() ?>" class="w-32 h-auto inline-block"></a>
-	<h1 class="text-2xl font-bold"><?php if(isset($title)){echo $title;}else{echo $site->title();} ?></h1>
+	<h1 class="text-2xl font-bold"><?php if(isset($title)){echo $title;}else{echo $page->title();} ?></h1>
 </header>
